8f6d193fcb7189cf7555cdf879bd826340031f9b
CadeLyons <cadelyons1236@gmail.com>
2021-10-12 16:53:32 -0500

    pushing changes to are-you-being-served

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -9,16 +9,38 @@ http.createServer(function(req, res) {
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 7: Get the start time for the race
-    
+    let d = new Date();
+    let startTime = d.getTime();
 
     // TODO 12: Make the whole thing parallel
     async.series( 
         // TODO 9: Supply an array of functions
         [
-            
+            function(callback) {
+                wrapper(callback);
+            },
+            function(callback) {
+                wrapper(callback);
+            },
+            function(callback) {
+                wrapper(callback);
+            },
+            function(callback) {
+                wrapper(callback);
+            }
         ],
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
+            res.write("Results:\n");
+            var victoryOrder = sortTogether(racers, results); 
+            for (i = 0; i < victoryOrder.length; i++) {
+                res.write(victoryOrder[i] + "\n");
+            }
+            let d = new Date();
+            let endTime = d.getTime();
+            let totalTime = endTime - startTime;
+            res.write(totalTime + "\n");
+            res.end();
 
         }
     );
@@ -27,7 +49,10 @@ http.createServer(function(req, res) {
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
-
+    setTimeout(function(){
+        let d = new Date();
+        callback(null, d.getTime());
+    }, Math.random()*1000);
 }
 
 function sortTogether(names, times) {
--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,1 +1,28 @@
 // requestServer.js file
+
+const http = require("http");
+const request = require("request");
+const port = 9155;
+
+var args = process.argv.slice(2);
+
+http.createServer(function(req, res) {
+    var url = args[0] ? args[0] : "<a default url>";
+    request(url, function(error, response, body) {
+        if (!body || !response || (error === null && response.statusCode !== 200)){
+            res.end("bad URL\n");
+            return;
+        }
+        if (response.statusCode === 200 && !error === true) {
+            res.writeHead(200, {"Content-Type" : "text/html"})
+            res.write(body);
+            res.end();
+        }
+        else {
+            res.writeHead(response.statusCode, {"Content-Type" : "text/plain"})
+            const errorString = error.toString()
+            res.write(errorString);
+            res.end();
+        }
+    })
+}).listen(9155);
