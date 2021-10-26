4d848d3ca1ca0e5d4f84c84ad9c2ff0e29b97327
CadeLyons <cadelyons1236@gmail.com>
2021-10-26 17:35:55 -0500

    first led bonus thing

---
+++ iot-projects/hardware-tests/tests/owen.js
@@ -0,0 +1,32 @@
+var onoff = require('onoff');
+
+var Gpio = onoff.Gpio,
+  led1 = new Gpio(16, 'out'),
+  led2 = new Gpio(21, 'out'),
+  interval;
+
+interval = setInterval(function () {
+  for (i = 0, i < 4; i++;) {
+    if (i % 2 === 0) {
+      setTimeout(led1.write(1, function() {
+        console.log("Changed LED 1 state to: " + 1);
+      }), 250 * i);
+    }
+    else {
+      led2.write(1, function() {
+        console.log("Changed LED 2 state to: " + 1);
+      });
+    } 
+  }
+}, 1001);
+
+process.on('SIGINT', function () {
+  clearInterval(interval);
+  led1.writeSync(0); 
+  led1.unexport();
+  led2.writeSync(0);
+  led2.unexport();
+  console.log('Bye, bye!');
+  process.exit();
+});
+
