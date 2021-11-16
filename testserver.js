bb3779f8941de7979eaf797b8a2f8c83243f770a
CadeLyons <cadelyons1236@gmail.com>
2021-11-16 17:54:36 -0600

    adding json2html files

--- testserver.js
+++ testserver.js
@@ -2,8 +2,20 @@ const http = require("http");
 let port = process.argv[2];
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"})
+    var json2html = require("node-json2html");
+
+    let data = {
+        "name": "daniel",
+        "age": 34,
+    };
+    let transform = {
+        '<>': 'h1', 'html': [
+            {'<>': 'li', 'html': '${name}'}
+        ]
+    };
+    let html = json2html.transform(data, transform);
+/*    res.writeHead(200, {"Content-Type": "text/html"})
     res.write("<h1> A </h1>")
-    res.write("<h2> neet</h2>")
-    res.end("<p> server! </p>")
+    res.write("<h2> neet</h2>") */
+    res.end(html.split('><').join('>\n<'));
 }).listen(port);
